44db1a889367e50b16b8898d68b50fff36badded
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-04-30 12:05:25 +0300

    Added class for jdbc operations

---
+++ eventuate-common-jdbc/src/main/java/io/eventuate/common/jdbc/EventuateCommonJdbcOperations.java
@@ -0,0 +1,56 @@
+package io.eventuate.common.jdbc;
+
+import io.eventuate.javaclient.spring.jdbc.EventuateSchema;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Optional;
+
+public class EventuateCommonJdbcOperations {
+  private JdbcTemplate jdbcTemplate;
+
+  public EventuateCommonJdbcOperations(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  public void insertIntoEventsTable(String eventId,
+                                    String entityId,
+                                    String eventData,
+                                    String eventType,
+                                    EventuateSchema eventuateSchema) {
+    insertIntoEventsTable(eventId, entityId, eventData, eventType, Optional.empty(), Optional.empty(), eventuateSchema);
+  }
+
+  public void insertIntoEventsTable(String eventId,
+                                    String entityId,
+                                    String eventData,
+                                    String eventType,
+                                    Optional<String> triggeringEvent,
+                                    Optional<String> metadata,
+                                    EventuateSchema eventuateSchema) {
+
+    jdbcTemplate.update(String.format("INSERT INTO %s (event_id, event_type, event_data, entity_type, entity_id, triggering_event, metadata) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            eventuateSchema.qualifyTable("events")),
+            eventId,
+            eventType,
+            eventData,
+            eventType,
+            entityId,
+            triggeringEvent.orElse(null),
+            metadata.orElse(null));
+  }
+
+  public void insertIntoMessageTable(String messageId,
+                                      String payload,
+                                      String destination,
+                                      String currentTimeInMillisecondsSql,
+                                      EventuateSchema eventuateSchema) {
+    String table = eventuateSchema.qualifyTable("message");
+
+    jdbcTemplate.update(String.format("insert into %s(id, destination, headers, payload, creation_time) values(?, ?, ?, ?, %s)",
+            table, currentTimeInMillisecondsSql),
+            messageId,
+            destination,
+            String.format("{\"ID\" : \"%s\"}", messageId),
+            payload);
+  }
+}
