7630e30c9a33c154d0bf492c778a592151eb3223
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-07-11 18:28:45 +0300

    Fixed eventuate common jdbc operations configuration.

--- eventuate-common-jdbc-spring/src/main/java/io/eventuate/common/jdbc/spring/EventuateCommonJdbcOperationsConfiguration.java
+++ eventuate-common-jdbc-spring/src/main/java/io/eventuate/common/jdbc/spring/EventuateCommonJdbcOperationsConfiguration.java
@@ -2,13 +2,11 @@ package io.eventuate.common.jdbc.spring;
 
 import io.eventuate.common.jdbc.EventuateCommonJdbcOperations;
 import io.eventuate.common.jdbc.EventuateJdbcStatementExecutor;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
-@EnableAutoConfiguration
 public class EventuateCommonJdbcOperationsConfiguration {
 
   @Bean
--- eventuate-common-jdbc-spring/src/test/java/io/eventuate/common/jdbc/spring/EventuateCommonJdbcOperationsTest.java
+++ eventuate-common-jdbc-spring/src/test/java/io/eventuate/common/jdbc/spring/EventuateCommonJdbcOperationsTest.java
@@ -7,16 +7,25 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 import java.util.*;
 
-@SpringBootTest(classes = EventuateCommonJdbcOperationsConfiguration.class)
+@SpringBootTest(classes = EventuateCommonJdbcOperationsTest.Config.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class EventuateCommonJdbcOperationsTest {
+
+  @Configuration
+  @EnableAutoConfiguration
+  @Import(EventuateCommonJdbcOperationsConfiguration.class)
+  public static class Config {
+  }
 
   @Autowired
   private EventuateCommonJdbcOperations eventuateCommonJdbcOperations;
