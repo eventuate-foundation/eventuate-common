cecfe5b1f9b7a3c7e576c127f9493727373a3345
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-12-20 11:06:57 +0300

    Fixed according CR

--- eventuate-common-in-memory-database-micronaut/src/main/java/io/eventuate/common/inmemorydatabase/EventuateCommonInMemoryDatabaseFactory.java
+++ eventuate-common-in-memory-database-micronaut/src/main/java/io/eventuate/common/inmemorydatabase/EventuateCommonInMemoryDatabaseFactory.java
@@ -1,23 +1,17 @@
 package io.eventuate.common.inmemorydatabase;
 
 import io.micronaut.context.annotation.Factory;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.inject.Singleton;
 import javax.sql.DataSource;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Factory
 public class EventuateCommonInMemoryDatabaseFactory {
 
   @Singleton
   public DataSource dataSource(List<EventuateDatabaseScriptSupplier> scripts) {
-    EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-    builder.setType(EmbeddedDatabaseType.H2);
-    scripts.stream().flatMap( s -> s.get().stream()).collect(Collectors.toSet()).forEach(builder::addScript);
-    return builder.build();
+    return new EventuateInMemoryDataSourceBuilder(scripts).build();
   }
 
 }
