0bff440a4ea22e6b6f38f75cedc5d46578eeba18
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-06-03 18:55:56 +0300

    Used unicode for mysql data columns.

--- mariadb/2.initialize-database.sql
+++ mariadb/2.initialize-database.sql
@@ -6,8 +6,8 @@ DROP Table IF Exists received_messages;
 CREATE TABLE message (
   id VARCHAR(767) PRIMARY KEY,
   destination LONGTEXT NOT NULL,
-  headers LONGTEXT NOT NULL,
-  payload LONGTEXT NOT NULL,
+  headers LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  payload LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   published SMALLINT DEFAULT 0,
   creation_time BIGINT
 );
