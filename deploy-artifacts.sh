32d37439d576453c385d7c28202380a3178d83c7
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-09-17 18:35:18 +0300

    #42: Use docker gradle plugin. Remove env variables. Fixed docker image publishing.

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -8,16 +8,13 @@ function deploy() {
   image=$1
 
   ./gradlew ${image}ComposePull || echo no image to pull
-  ./gradlew ${image}ComposeBuild ${image}ImageComposePush
+  ./gradlew ${image}ComposeBuild ${image}ComposePush
 }
-
-$PREFIX docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
 export DOCKER_IMAGE_TAG=$VERSION
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-deploy "zookeeper"
 deploy "postgres"
 deploy "mysql"
 deploy "mariadb"
