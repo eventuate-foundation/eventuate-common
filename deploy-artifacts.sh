8fe909f59029d04c6925c1a18e52636409fbf150
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-09-17 15:49:39 +0300

    #42: Use docker gradle plugin. Remove env variables. Added missed docker image publishing.

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -1,3 +1,26 @@
 #! /bin/bash -e
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+VERSION=$BRANCH
+
+function deploy() {
+  image=$1
+
+  ./gradlew ${image}ComposePull || echo no image to pull
+  ./gradlew ${image}ComposeBuild ${image}ImageComposePush
+}
+
+$PREFIX docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
+
+export DOCKER_IMAGE_TAG=$VERSION
+
+docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
+
+deploy "zookeeper"
+deploy "postgres"
+deploy "mysql"
+deploy "mariadb"
+deploy "mssql"
+
 ./gradlew  publishEventuateArtifacts
