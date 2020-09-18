a691ea45909ff74f9c19d527402c0859931752ce
Chris Richardson <chris@chrisrichardson.net>
2020-09-17 17:47:51 -0700

    #42 use io.eventuate.plugins.gradle.publish.EventuatePublishDockerImages to publish images

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -1,23 +1,7 @@
 #! /bin/bash -e
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-VERSION=$BRANCH
-
-function deploy() {
-  image=$1
-
-  ./gradlew ${image}ComposePull || echo no image to pull
-  ./gradlew ${image}ComposeBuild ${image}ComposePush
-}
-
-export DOCKER_IMAGE_TAG=$VERSION
-
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-deploy "postgres"
-deploy "mysql"
-deploy "mariadb"
-deploy "mssql"
-
 ./gradlew  publishEventuateArtifacts
+
+./gradlew publishEventuateDockerImages
