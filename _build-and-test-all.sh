52b2e241b92b7d60f4366276f38ae6b241f19cac
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-09-28 13:27:42 +0300

    Optimized test script.

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -11,6 +11,16 @@ if [ "$1" = "--clean" ] ; then
   shift
 fi
 
+function testJdbc() {
+  ${docker}Up
+
+  ./gradlew $* :eventuate-common-micronaut-data-jdbc:cleanTest :eventuate-common-micronaut-data-jdbc:test \
+  :eventuate-common-micronaut-spring-jdbc:cleanTest :eventuate-common-micronaut-spring-jdbc:test \
+  :eventuate-common-spring-jdbc:cleanTest :eventuate-common-spring-jdbc:test
+
+  ${docker}Down
+}
+
 docker="./gradlew ${DATABASE?}Compose"
 ${docker}Down
 
@@ -22,63 +32,30 @@ echo ""
 echo "TESTING REGULAR DATABASE"
 echo ""
 
-
-
 ${docker}UP
-
 ./gradlew $* cleanTest build
-
 ${docker}Down
-
-
 
 echo ""
 echo "TESTING DATABASE WITH DBID WITH APPLICATION ID GENERATION"
 echo ""
 
-
-
 export USE_DB_ID=true
-${docker}Up
-
-./gradlew $* :eventuate-common-micronaut-data-jdbc:cleanTest :eventuate-common-micronaut-data-jdbc:test \
-:eventuate-common-micronaut-spring-jdbc:cleanTest :eventuate-common-micronaut-spring-jdbc:test \
-:eventuate-common-spring-jdbc:cleanTest :eventuate-common-spring-jdbc:test
-
-${docker}Down
-
-
+testJdbc
 
 echo ""
 echo "TESTING DATABASE WITH DBID WITH DATABASE ID GENERATION"
 echo ""
 
-
-
 export EVENTUATE_OUTBOX_ID=1
-${docker}Up
-
-./gradlew $* :eventuate-common-micronaut-data-jdbc:cleanTest :eventuate-common-micronaut-data-jdbc:test \
-:eventuate-common-micronaut-spring-jdbc:cleanTest :eventuate-common-micronaut-spring-jdbc:test \
-:eventuate-common-spring-jdbc:cleanTest :eventuate-common-spring-jdbc:test
-
-${docker}Down
-
-
+testJdbc
 
 echo ""
 echo "TESTING DATABASE WITH JSON SUPPORT"
 echo ""
 
-
-
 unset USE_DB_ID
 unset EVENTUATE_OUTBOX_ID
 export USE_JSON_PAYLOAD_AND_HEADERS=true
-${docker}Up
+testJdbc
 
-./gradlew $* :eventuate-common-micronaut-data-jdbc:cleanTest :eventuate-common-micronaut-data-jdbc:test \
-:eventuate-common-micronaut-spring-jdbc:cleanTest :eventuate-common-micronaut-spring-jdbc:test \
-:eventuate-common-spring-jdbc:cleanTest :eventuate-common-spring-jdbc:test
-
-${docker}Down
