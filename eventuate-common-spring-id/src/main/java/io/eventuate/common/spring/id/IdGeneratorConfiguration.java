34c0cd4f434dea27aa5a3c044318379743b68318
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-10-14 21:09:45 +0300

    Fixed application id generator condition.

--- eventuate-common-spring-id/src/main/java/io/eventuate/common/spring/id/IdGeneratorConfiguration.java
+++ eventuate-common-spring-id/src/main/java/io/eventuate/common/spring/id/IdGeneratorConfiguration.java
@@ -6,20 +6,21 @@ import io.eventuate.common.id.DatabaseIdGenerator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class IdGeneratorConfiguration {
 
   @Bean
-  @ConditionalOnProperty(name = "eventuate.outbox.id", matchIfMissing = true)
-  public IdGenerator idGenerator() {
+  @Conditional(ApplicationIdGeneratorCondition.class)
+  public IdGenerator applicationIdGenerator() {
     return new ApplicationIdGenerator();
   }
 
   @Bean
   @ConditionalOnProperty(name = "eventuate.outbox.id")
-  public IdGenerator idGenerator(@Value("${eventuate.outbox.id:#{null}}") long id) {
+  public IdGenerator databaseIdGenerator(@Value("${eventuate.outbox.id:#{null}}") long id) {
     return new DatabaseIdGenerator(id);
   }
 }
