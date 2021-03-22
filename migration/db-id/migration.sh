888ee9f6004f12ae51b9fcbbb69b1235999b5b1a
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2021-03-22 21:07:06 +0300

    Refactored db id migration script.

--- migration/db-id/migration.sh
+++ migration/db-id/migration.sh
@@ -18,24 +18,26 @@ get_db_id_migration_path () {
   echo "${DB_ID_MIGRATION_REPOSITORY}/${version}"
 }
 
-if [ "${DATABASE}" == "mysql" ]; then
-  curl -s $(get_db_id_migration_path)/mysql/4.initialize-database-db-id.sql &> /dev/stdout | ./mysql-cli.sh -i
-elif [ "${DATABASE}" == "postgres" ]; then
-  curl -s $(get_db_id_migration_path)/postgres/5.initialize-database-db-id.sql &> /dev/stdout | ./postgres-cli.sh -i
-elif [ "${DATABASE}" == "mssql" ]; then
-  rm -rf migration
-  migration_file=migration/migration.sql
-  migration_tool=docker-compose-mssql-migration-tool.yml
-  migration_entrypoint=migration/entrypoint.sh
+db_id_migration_path=$(get_db_id_migration_path)
 
-  curl $(get_db_id_migration_path)/mssql/4.setup-db-id.sql --output ${migration_file} --create-dirs
-  curl "$(get_db_id_migration_path)/migration/db-id/mssql/${migration_tool}" --output ${migration_tool}
-  curl "$(get_db_id_migration_path)/migration/db-id/mssql/entrypoint.sh" --output ${migration_entrypoint}
+if [ "${DATABASE}" == "mysql" ]; then
+  curl -s ${db_id_migration_path}/mysql/4.initialize-database-db-id.sql &> /dev/stdout | ./mysql-cli.sh -i
+elif [ "${DATABASE}" == "postgres" ]; then
+  curl -s ${db_id_migration_path}/postgres/5.initialize-database-db-id.sql &> /dev/stdout | ./postgres-cli.sh -i
+elif [ "${DATABASE}" == "mssql" ]; then
+  rm -rf tmp-migration
+  migration_file=tmp-migration/migration.sql
+  migration_tool=docker-compose-mssql-migration-tool.yml
+  migration_entrypoint=tmp-migration/entrypoint.sh
+
+  curl ${db_id_migration_path}/mssql/4.setup-db-id.sql --output ${migration_file} --create-dirs
+  curl ${db_id_migration_path}/migration/db-id/mssql/${migration_tool} --output ${migration_tool}
+  curl ${db_id_migration_path}/migration/db-id/mssql/entrypoint.sh --output ${migration_entrypoint}
   docker-compose -f docker-compose-mssql-polling.yml -f ${migration_tool} up --build --no-deps mssql-migration
   docker-compose -f docker-compose-mssql-polling.yml -f ${migration_tool} stop mssql-migration
   docker-compose -f docker-compose-mssql-polling.yml -f ${migration_tool} rm -f mssql-migration
 
-  rm -rf migration
+  rm -rf tmp-migration
   rm -rf ${migration_tool}
 
 else
