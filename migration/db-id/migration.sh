da0b1dfe0411a68ccc9fe4a916db9678326fb295
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2021-03-23 12:19:23 +0300

    Created DB_ID_MIGRATION_DIR env variable.

--- migration/db-id/migration.sh
+++ migration/db-id/migration.sh
@@ -6,6 +6,13 @@ if [ -z "$DB_ID_MIGRATION_REPOSITORY" ] ; then
   export DB_ID_MIGRATION_REPOSITORY=https://raw.githubusercontent.com/eventuate-foundation/eventuate-common
 fi
 
+if [ -z "$DB_ID_MIGRATION_DIR" ] ; then
+  # docker-compose understand path only with "./" part.
+  # Also it should be part of the variable.
+  # docker-compose cannot parse this ./${DB_ID_MIGRATION_DIR}
+  export DB_ID_MIGRATION_DIR=./tmp-migration
+fi
+
 get_db_id_migration_path () {
   search="eventuateCommonVersion="
   version_line="$(grep $search ./gradle.properties)"
@@ -25,10 +32,10 @@ if [ "${DATABASE}" == "mysql" ]; then
 elif [ "${DATABASE}" == "postgres" ]; then
   curl -s ${db_id_migration_path}/postgres/5.initialize-database-db-id.sql &> /dev/stdout | ./postgres-cli.sh -i
 elif [ "${DATABASE}" == "mssql" ]; then
-  rm -rf tmp-migration
-  migration_file=tmp-migration/migration.sql
+  rm -rf ${DB_ID_MIGRATION_DIR}
+  migration_file=${DB_ID_MIGRATION_DIR}/migration.sql
   migration_tool=docker-compose-mssql-migration-tool.yml
-  migration_entrypoint=tmp-migration/entrypoint.sh
+  migration_entrypoint=${DB_ID_MIGRATION_DIR}/entrypoint.sh
 
   curl ${db_id_migration_path}/mssql/4.setup-db-id.sql --output ${migration_file} --create-dirs
   curl ${db_id_migration_path}/migration/db-id/mssql/${migration_tool} --output ${migration_tool}
@@ -37,7 +44,7 @@ elif [ "${DATABASE}" == "mssql" ]; then
   docker-compose -f docker-compose-mssql-polling.yml -f ${migration_tool} stop mssql-migration
   docker-compose -f docker-compose-mssql-polling.yml -f ${migration_tool} rm -f mssql-migration
 
-  rm -rf tmp-migration
+  rm -rf ${DB_ID_MIGRATION_DIR}
   rm -rf ${migration_tool}
 
 else
