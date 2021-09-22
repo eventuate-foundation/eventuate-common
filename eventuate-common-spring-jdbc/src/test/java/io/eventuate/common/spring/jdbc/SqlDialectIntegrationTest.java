27a3ba802eb0fb8cd84b14fbb1d3d8d48b62c3a4
Artem Sidorkin <Artem.Sidorkin@digitalarrowtech.com>
2021-09-22 11:11:01 +0300

    Added primary key test for received_messages table.

--- eventuate-common-spring-jdbc/src/test/java/io/eventuate/common/spring/jdbc/SqlDialectIntegrationTest.java
+++ eventuate-common-spring-jdbc/src/test/java/io/eventuate/common/spring/jdbc/SqlDialectIntegrationTest.java
@@ -28,6 +28,8 @@ import static io.eventuate.common.jdbc.EventuateJdbcOperationsUtils.EVENT_APPLIC
 import static io.eventuate.common.jdbc.EventuateJdbcOperationsUtils.EVENT_AUTO_GENERATED_ID_COLUMN;
 import static io.eventuate.common.jdbc.EventuateJdbcOperationsUtils.MESSAGE_APPLICATION_GENERATED_ID_COLUMN;
 import static io.eventuate.common.jdbc.EventuateJdbcOperationsUtils.MESSAGE_AUTO_GENERATED_ID_COLUMN;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singleton;
 
 @SpringBootTest(classes = SqlDialectIntegrationTest.Config.class)
 @RunWith(SpringRunner.class)
@@ -132,23 +134,28 @@ public class SqlDialectIntegrationTest {
 
   @Test
   public void testEventsPrimaryKeyColumn() throws SQLException {
-    assertPrimaryKeyColumnEquals("events", useDbId
+    assertPrimaryKeyColumnEquals("events", singleton(useDbId
             ? EVENT_AUTO_GENERATED_ID_COLUMN
-            : EVENT_APPLICATION_GENERATED_ID_COLUMN);
+            : EVENT_APPLICATION_GENERATED_ID_COLUMN));
   }
 
   @Test
   public void testMessagePrimaryKeyColumn() throws SQLException {
-    assertPrimaryKeyColumnEquals("message", useDbId
+    assertPrimaryKeyColumnEquals("message", singleton(useDbId
             ? MESSAGE_AUTO_GENERATED_ID_COLUMN
-            : MESSAGE_APPLICATION_GENERATED_ID_COLUMN);
+            : MESSAGE_APPLICATION_GENERATED_ID_COLUMN));
   }
 
-  private void assertPrimaryKeyColumnEquals(String table, String expectedKeyColumn) throws SQLException {
+  @Test
+  public void testReceivedMessagePrimaryKeyColumn() throws SQLException {
+    assertPrimaryKeyColumnEquals("received_messages", new HashSet<>(asList("message_id", "consumer_id")));
+  }
+
+  private void assertPrimaryKeyColumnEquals(String table, Set<String> expectedKeyColumns) throws SQLException {
     List<String> pkColumns = getDialect()
             .getPrimaryKeyColumns(dataSource, dataSourceUrl, new SchemaAndTable(EventuateSchema.DEFAULT_SCHEMA, table));
 
-    Assert.assertEquals(Collections.singletonList(expectedKeyColumn), pkColumns);
+    Assert.assertEquals(expectedKeyColumns, new HashSet<>(pkColumns));
   }
 
   private void assertAllRowsHaveTheSameEventType(List<Map<String, Object>> rows, String eventType) {
