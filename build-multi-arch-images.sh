3c38a8134ed5bdf5b8022994831d97db89d897fb
Chris Richardson <chris@chrisrichardson.net>
2022-01-27 13:07:14 -0800

    Configure CircleCI to build multi-arch MySQL8

---
+++ build-multi-arch-images.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+MYSQL8_MULTI_ARCH_IMAGE=eventuateio/eventuate-mysql8:test-build-${CIRCLE_SHA1?}
+
+docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
+
+docker buildx build --platform linux/amd64,linux/arm64 \
+  -t $MYSQL8_MULTI_ARCH_IMAGE \
+  -f Dockerfile-mysql/mysql8-multi-arch \
+  --push \
+  mysql
