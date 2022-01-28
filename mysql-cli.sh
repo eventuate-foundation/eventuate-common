b0a584204d78554c8929cafe4c9c37ac0a4b617b
Chris Richardson <chris@chrisrichardson.net>
2022-01-28 10:48:55 -0800

    Changes to enable local development with MySQL8 on Arm/M1 (single arch image only) - #106
    * Use multi-arch base images for MySQL8 and Zookeeper
    * Replace environment variables with Gradle properties to address Gradle Daemon issue

--- mysql-cli.sh
+++ mysql-cli.sh
@@ -1,23 +1,20 @@
 #! /bin/bash -e
 
-if [ -z "$MYSQL_PORT" ]; then
-    export MYSQL_PORT=3306
-fi
+NETWORK=${PWD##*/}_default
 
 if [ -z "$DATABASE" ] ; then
-  export DATABASE=mysql
+  DATABASE=mysql
 fi
 
-if [ "${DATABASE}" == "mysql" ]; then
-  export mysqlimage="mysql:5.7.13"
-elif [ "${DATABASE}" == "mariadb" ]; then
-  export mysqlimage="mariadb:10.3.8"
-elif [ "${DATABASE}" == "mysql8" ]; then
-  export mysqlimage="mysql:8.0.22"
+if [ "${DATABASE}" == "mysql" ] ; then
+  dockerfile=Dockerfile
+else
+  dockerfile=Dockerfile-$DATABASE
 fi
 
-docker run $* \
-   --name mysqlterm --network=${PWD##*/}_default --rm \
-   -e HOST=${DATABASE} -e MYSQL_PORT=$MYSQL_PORT \
-   ${mysqlimage}  \
-   sh -c 'exec mysql -h"$HOST" -P"$MYSQL_PORT" -uroot -prootpassword -o eventuate'
+mysqlimage=$(head -1 mysql/$dockerfile | sed -e 's/FROM *//')
+
+docker run ${1:--it} \
+   --name mysqlterm --network=$NETWORK --rm -e HOST=${DATABASE} -e MYSQL_PORT=${MYSQL_PORT:-3306} \
+   ${mysqlimage} \
+   sh -c 'exec mysql -h$HOST  -P"$MYSQL_PORT" -uroot -prootpassword -o eventuate'
