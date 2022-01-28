88226b90841f909368fd56007af5689fb44bb724
Chris Richardson <chris@chrisrichardson.net>
2022-01-28 10:25:42 -0800

    Ensure that mysql-cli.sh uses the correct image and hostname

--- mysql-cli.sh
+++ mysql-cli.sh
@@ -2,7 +2,19 @@
 
 NETWORK=${PWD##*/}_default
 
+if [ -z "$DATABASE" ] ; then
+  DATABASE=mysql
+fi
+
+if [ "${DATABASE}" == "mysql" ] ; then
+  dockerfile=Dockerfile
+else
+  dockerfile=Dockerfile-$DATABASE
+fi
+
+mysqlimage=$(head -1 mysql/$dockerfile | sed -e 's/FROM *//')
+
 docker run ${1:--it} \
-   --name mysqlterm --network=$NETWORK --rm \
-   mysql/mysql-server:8.0.27-1.2.6-server \
-   sh -c 'exec mysql -hmysql8  -uroot -prootpassword -o eventuate'
+   --name mysqlterm --network=$NETWORK --rm -e HOST=${DATABASE} -e MYSQL_PORT=${MYSQL_PORT:-3306} \
+   ${mysqlimage} \
+   sh -c 'exec mysql -h$HOST  -P"$MYSQL_PORT" -uroot -prootpassword -o eventuate'
