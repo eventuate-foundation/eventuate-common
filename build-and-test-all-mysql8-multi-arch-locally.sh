bf7a47da059e2d1f6d978bd56677f980fbf29592
Chris Richardson <chris@chrisrichardson.net>
2022-01-28 21:58:07 -0800

    #106 Support building multi-arch Zookeeper locally

--- build-and-test-all-mysql8-multi-arch-locally.sh
+++ build-and-test-all-mysql8-multi-arch-locally.sh
@@ -3,7 +3,9 @@
 export DATABASE=mysql8-multi-arch
 
 ./mysql/build-docker-mysql-8-multi-arch.sh
+./zookeeper/build-docker-zookeeper-multi-arch.sh
 
 docker pull localhost:5002/eventuate-mysql8:multi-arch-local-build
+docker pull localhost:5002/eventuate-zookeeper:multi-arch-local-build
 
 ./_build-and-test-all.sh
---
+++ zookeeper/build-docker-zookeeper-multi-arch.sh
@@ -0,0 +1,11 @@
+#! /bin/bash -e
+
+SCRIPT_DIR=$(cd $( dirname "${BASH_SOURCE[0]}" ) ; pwd)
+
+docker-compose -f $SCRIPT_DIR/../docker-compose-registry.yml --project-name eventuate-common-registry up -d registry
+
+docker buildx build --platform linux/amd64,linux/arm64 \
+  -t host.docker.internal:5002/eventuate-zookeeper:multi-arch-local-build \
+  -f $SCRIPT_DIR/Dockerfile \
+  --output=type=image,push=true,registry.insecure=true \
+  $SCRIPT_DIR
