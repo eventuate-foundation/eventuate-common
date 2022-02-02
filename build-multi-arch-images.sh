a2525472d97f889c388865a3120ad8fed5551520
Chris Richardson <chris@chrisrichardson.net>
2022-02-01 21:48:45 -0800

    Attempt to fix qemu problem

--- build-multi-arch-images.sh
+++ build-multi-arch-images.sh
@@ -2,5 +2,12 @@
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
+# failed to solve: process "/dev/.buildkit_qemu_emulator
+# https://github.com/docker/buildx/issues/493#issuecomment-754834977
+# https://github.com/tonistiigi/binfmt#installing-emulators
+
+docker run --privileged --rm tonistiigi/binfmt --uninstall qemu-aarch64
+docker run --privileged --rm tonistiigi/binfmt --install arm64,arm
+
 ./mysql/build-docker-mysql-8-multi-arch.sh
 ./zookeeper/build-docker-zookeeper-multi-arch.sh
