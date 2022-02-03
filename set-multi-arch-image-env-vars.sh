9bdc665662362668a62cf32eae64a88ac9cde2cd
Chris Richardson <chris@chrisrichardson.net>
2022-02-03 06:58:55 -0800

    Publish multi architecture MySQL8 and Zookeeper

--- set-multi-arch-image-env-vars.sh
+++ set-multi-arch-image-env-vars.sh
@@ -1,5 +1,6 @@
 
 
-export MYSQL8_MULTI_ARCH_IMAGE=eventuateio/eventuate-mysql8:test-build-${CIRCLE_SHA1?}
-export ZOOKEEPER_MULTI_ARCH_IMAGE=eventuateio/eventuate-zookeeper:test-build-${CIRCLE_SHA1?}
+export MULTI_ARCH_TAG=test-build-${CIRCLE_SHA1?}
+export MYSQL8_MULTI_ARCH_IMAGE=eventuateio/eventuate-mysql8:$MULTI_ARCH_TAG
+export ZOOKEEPER_MULTI_ARCH_IMAGE=eventuateio/eventuate-zookeeper:$MULTI_ARCH_TAG
 export BUILDX_PUSH_OPTIONS=--push
