5cdec129fe1aa3dcbefaf030ee5ba33a3a69a88b
Chris Richardson <chris@chrisrichardson.net>
2022-02-03 10:03:46 -0800

    Set multi-arch envars for publishing

--- deploy-multi-arch.sh
+++ deploy-multi-arch.sh
@@ -18,7 +18,7 @@ docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
 retag() {
   BASE=$1
-  IMAGE=${BASE}:${MULTI_ARCH_TAG}
+  IMAGE=${BASE}:${MULTI_ARCH_TAG?}
   TARGET_IMAGE=$BASE:$TARGET_TAG
 
   echo Retagging $IMAGE $TARGET_IMAGE
