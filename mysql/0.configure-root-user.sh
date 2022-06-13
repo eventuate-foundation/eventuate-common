75231e1b0cb959143ec73783315f0c6265693dbc
Chris Richardson <chris@chrisrichardson.net>
2022-06-13 11:18:44 +0900

    #115 typo in mysql8 initialization script

--- mysql/0.configure-root-user.sh
+++ mysql/0.configure-root-user.sh
@@ -8,7 +8,7 @@ fi
 cat >> /docker-entrypoint-initdb.d/5.configure-users.sql <<END
 CREATE USER 'root'@'${MYSQL_ROOT_HOST}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 GRANT ALL ON *.* TO 'root'@'${MYSQL_ROOT_HOST}' WITH GRANT OPTION ;
-GRANT PROXY ON ''@'' TO 'root'@'${MYSQL_ROOT_HOST}' WITH GRANT OPTION ;"
+GRANT PROXY ON ''@'' TO 'root'@'${MYSQL_ROOT_HOST}' WITH GRANT OPTION ;
 END
 
 echo start /docker-entrypoint-initdb.d/5.configure-users.sql
