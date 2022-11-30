9f1c873ab3cab3be69c7eaa82d712830af09e174
Chris Richardson <chris@chrisrichardson.net>
2022-11-30 13:53:44 +0900

    Fixed typo

--- postgres-cli.sh
+++ postgres-cli.sh
@@ -11,5 +11,5 @@ fi
 docker run $* \
    --name postgresterm --network=${PWD##*/}_default --rm \
    -e POSTGRES_PORT=$POSTGRES_PORT -e PGPASSWORD=eventuate -e POSTGRES_HOST=${DATABASE/-multi-arch/} \
-   postgres:postgres:12 \
+   postgres:12 \
    sh -c 'exec psql -p $POSTGRES_PORT -h $POSTGRES_HOST -U eventuate'
