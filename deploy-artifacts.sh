df741413594ccbf634d40e1ecdb3dd85e875a4a3
Chris Richardson <chris@chrisrichardson.net>
2022-12-02 12:05:24 -0800

    Use --parallel for publishEventuateArtifacts

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -2,5 +2,5 @@
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-./gradlew publishEventuateArtifacts
+./gradlew --parallel publishEventuateArtifacts
 ./gradlew publishEventuateDockerImages
