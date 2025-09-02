2ce89e288b42a9e8f35fcf76b3756dedd03359af
Chris Richardson <chris@chrisrichardson.net>
2025-09-02 11:12:07 -0700

    Only create WAL replication slot if it doesn't already exist

--- eventuate-common-flyway/src/main/resources/flyway/postgresql/V3__eventuate_postgres_wal.sql
+++ eventuate-common-flyway/src/main/resources/flyway/postgresql/V3__eventuate_postgres_wal.sql
@@ -1,1 +1,4 @@
-SELECT * FROM pg_create_logical_replication_slot('eventuate_slot', 'wal2json');
+SELECT * FROM pg_create_logical_replication_slot('eventuate_slot', 'wal2json')
+WHERE NOT EXISTS (
+    SELECT 1 FROM pg_replication_slots WHERE slot_name = 'eventuate_slot'
+);
